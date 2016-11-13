6d5e4d0ce4562f973260f044da1908e3933a27c3
HyperFlash-Studios <hypersonicak@gmail.com>
2016-11-12 23:33:13 -0600

    Update PostReader.js

--- PostReader.js
+++ PostReader.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var divCount =  $('div[class="counter"]:contains:('48')');
+  var divCount =  $('div[class="counter"]:contains(\'48\')');
   var ofOlde = [];
   for (var i = 0; i < divCount.length; i++) {
   ofOlde.push(divCount);
