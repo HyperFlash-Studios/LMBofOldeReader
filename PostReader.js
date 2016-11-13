1f44fbc5d47ebb722306216fe433ceef52a5870d
HyperFlash-Studios <hypersonicak@gmail.com>
2016-11-12 23:40:39 -0600

    Update PostReader.js

--- PostReader.js
+++ PostReader.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
-  var divCount =  $('div[class="counter"]:contains(\'48\')');
+  var divCount =  $('div[class="counter"]:contains(\'<= 48\')');
   var ofOlde = [];
   for (var i = 0; i < divCount.length; i++) {
   ofOlde.push(divCount);
+    console.log(ofOlde);
 }
+  if(divCount.length === 0) {
+    console.log("There are no users with this many posts!");
+  }
 });
