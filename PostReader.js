3590430ffa0c45000a2feb646a863a0e9f6e6093
Samantha Nguyen <SamanthaNguyen@users.noreply.github.com>
2017-01-14 21:18:27 -0600

    code nitpicking to fit MediaWiki coding conventions

--- PostReader.js
+++ PostReader.js
@@ -1,21 +1,20 @@
-$(document).ready(function() {
-  var divCount =  $('div[class="counter"]:contains(\'<= 48\')');
-  var ofOlde = [];
-  for (var i = 0; i < divCount.length; i++) {
-  ofOlde.push(divCount);
-    console.log(ofOlde);
-}
-  if(divCount.length === 0) {
-    console.log("There are no users with this many posts!");
-  }
-});
-
+$( document ).ready( function() {
+	var divCount =  $( 'div[class="counter"]:contains(\'<= 48\')' );
+	var ofOlde = [];
+	for (var i = 0; i < divCount.length; i++) {
+		ofOlde.push(divCount);
+		console.log(ofOlde);
+	}
+	if( divCount.length === 0 ) {
+		console.log( 'There are no users with this many posts!' );
+	}
+} );
 
 //dooot
-var ofOldeUser = $('span[class="UserName lia-user-name"]:last-child').text();
-var postCount = parseInt($('div[class="counter"]').text().replace(" posts",""));
-  if (postCount >= 48) {
-  console.log(ofOldeUser);
-  } else {
-  console.log("There are no users with this many posts!");
-  }
+var ofOldeUser = $( 'span[class="UserName lia-user-name"]:last-child' ).text();
+var postCount = parseInt( $( 'div[class="counter"]').text().replace( " posts","" ) );
+if ( postCount >= 48 ) {
+	console.log( ofOldeUser);
+} else {
+	console.log("There are no users with this many posts!");
+}
